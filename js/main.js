e309bffd37706c46659f51302bb6e6936f1519f7
Carolina Passos <carolina.pos@gmail.com>
2017-04-07 17:07:33 -0300

    Add map interaction

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
 
+    $(document).on("mousemove", function(event) {
+        $('#tooltip').css('top', event.pageY + 20);
+        $('#tooltip').css('left', event.pageX + 20);
+    });
+
+    $('svg g').each(function(event) {
+        var name = $(this).attr('class');
+        $(this).hover(
+            function(event) {
+                $(this)
+                    .find('path')
+                        .css('stroke', '#000')
+                        .css('stroke-width', '2');
+                $(this)
+                    .css('z-index', 1);
+                $('#tooltip').html(name);
+                $('#tooltip').show();
+            },
+            function(event) {
+                $(this)
+                    .find('path')
+                        .css('stroke', '#FFF')
+                        .css('stroke-width', '0.737');
+            }
+        );
+    });
+    $('svg').mouseout(function(event) {
+        $('#tooltip').hide();
+    });
+
 });
