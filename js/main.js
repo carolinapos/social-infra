108f7ec7f828afa75fa6c2ac7cb01d6b48009ad1
Carolina Passos <carolina.pos@gmail.com>
2017-04-08 15:05:45 -0300

    Add data maps

--- js/main.js
+++ js/main.js
@@ -6,20 +6,28 @@ $(document).ready(function(){
     });
 
     $('svg g').each(function(event) {
-        var name = $(this).attr('class');
+        var title = $(this).data('title');
+        var healthUnits = $(this).data('health-units');
+        var educationUnits = $(this).data('education-units');
+        var culturalUnits = $(this).data('cultural-units');
+        var sportsUnits = $(this).data('sports-units');
         $(this).hover(
             function(event) {
-                $(this)
+                $('.' + this.className.baseVal)
                     .find('path')
                         .css('stroke', '#000')
                         .css('stroke-width', '2');
                 $(this)
                     .css('z-index', 1);
-                $('#tooltip').html(name);
+                $('#tooltip h1').html(title);
+                $('#tooltip .health-units').html(healthUnits);
+                $('#tooltip .education-units').html(educationUnits);
+                $('#tooltip .cultural-units').html(culturalUnits);
+                $('#tooltip .sports-units').html(sportsUnits);
                 $('#tooltip').show();
             },
             function(event) {
-                $(this)
+                $('.' + this.className.baseVal)
                     .find('path')
                         .css('stroke', '#FFF')
                         .css('stroke-width', '0.737');
